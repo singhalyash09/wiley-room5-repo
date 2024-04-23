622cd85f0e079a71777337d71fed3a8c3836e79b
Yash Agarwal <agarwalyash493@gmail.com>
2024-04-24 00:13:06 +0530

    Added Boolean Function for Leap Year Checker

--- com/practice/LeapYearChecker.java
+++ com/practice/LeapYearChecker.java
@@ -9,10 +9,14 @@ public class LeapYearChecker {
         System.out.println("Enter a year to check whether the Year is a Leap Year or not");
         int year = scan.nextInt();
 
+        System.out.println(isLeapYear(year));
+    }
+
+    static boolean isLeapYear(int year){
         if(year%4==0 && (year%100!=0 || year%400==0)){
-            System.out.println("The Entered Year "+year+ " is a Leap Year.\n Yayy! We got an Extra Day");
+            return true;
         }else {
-            System.out.println("The Entered Year is not a Leap Year.");
+            return false;
         }
     }
 }
